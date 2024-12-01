9a097f53c05b3b54bd6ada5ba7657874d157fcb4
Igor Storchevoy <hellsgor@gmail.com>
2024-12-01 16:27:07 +0400

    Update[App]: added input

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 
 function App() {
   const [likes, setLikes] = useState(5);
+  const [value, setValue] = useState('ТЕКСТ В ИНПУТЕ');
 
   function increment() {
     setLikes(likes + 1);
@@ -16,6 +17,13 @@ function App() {
       <h1>{likes}</h1>
       <button onClick={increment}>Increment</button>
       <button onClick={decrement}>Decrement</button>
+
+      <h1>{value}</h1>
+      <input
+        type="text"
+        value={value}
+        onChange={(event) => setValue(event.target.value)}
+      />
     </>
   );
 }
