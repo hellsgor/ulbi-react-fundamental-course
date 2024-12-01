cce66cdf0de0bdd206b5a94832a58405e0e77509
Igor Storchevoy <hellsgor@gmail.com>
2024-12-02 01:16:15 +0400

    Feat[App]: using controlled and uncontrolled components

--- src/App.tsx
+++ src/App.tsx
@@ -3,18 +3,33 @@ import './styles/App.css';
 import { PostList } from './components/PostList/PostList';
 import { Button } from './components/UI/Button/Button';
 import { TextInput } from './components/UI/TextInput/TextInput';
+import { useRef } from 'react';
 
 export function App() {
+  const postTitleInput = useRef<HTMLInputElement | null>(null);
+  const postTextInput = useRef<HTMLInputElement | null>(null);
+
+  function addPost() {
+    console.log(postTitleInput.current?.value);
+    console.log(postTextInput.current?.value);
+  }
+
   return (
     <div className="App">
       <form style={{ display: 'flex', flexDirection: 'column', rowGap: '8px' }}>
         <TextInput
+          ref={postTitleInput}
           placeholder="Заголовок поста"
           type="text"
           name="post-title"
         />
-        <TextInput placeholder="Текст поста" type="text" name="post-text" />
-        <Button>
+        <TextInput
+          ref={postTextInput}
+          placeholder="Текст поста"
+          type="text"
+          name="post-text"
+        />
+        <Button onClick={addPost} type="button">
           <span>Создать пост</span>
         </Button>
       </form>
--- src/components/UI/TextInput/TextInput.tsx
+++ src/components/UI/TextInput/TextInput.tsx
@@ -1,8 +1,9 @@
-import { FC, InputHTMLAttributes } from 'react';
+import { forwardRef, InputHTMLAttributes } from 'react';
 import classes from './TextInput.module.css';
 
-export const TextInput: FC<InputHTMLAttributes<HTMLInputElement>> = ({
-  ...props
-}) => {
-  return <input {...props} className={classes['text-input']} />;
-};
+export const TextInput = forwardRef<
+  HTMLInputElement,
+  InputHTMLAttributes<HTMLInputElement>
+>((props, ref) => {
+  return <input {...props} ref={ref} className={classes['text-input']} />;
+});
