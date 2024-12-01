c94bf8fc6f990968eb55787a67b3d72eaeeaa11a
Igor Storchevoy <hellsgor@gmail.com>
2024-12-01 16:39:16 +0400

    Feat[Counter]: create component

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,14 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
+import Counter from './components/Counter';
 
 function App() {
-  const [likes, setLikes] = useState(5);
   const [value, setValue] = useState('ТЕКСТ В ИНПУТЕ');
-
-  function increment() {
-    setLikes(likes + 1);
-  }
-
-  function decrement() {
-    setLikes(likes - 1);
-  }
 
   return (
     <>
-      <h1>{likes}</h1>
-      <button onClick={increment}>Increment</button>
-      <button onClick={decrement}>Decrement</button>
-
-      <h1>{value}</h1>
-      <input
-        type="text"
-        value={value}
-        onChange={(event) => setValue(event.target.value)}
-      />
+      <Counter />
+      <Counter />
+      <Counter />
     </>
   );
 }
