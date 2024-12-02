25d5d03e0bbd4f9fb013ac5a0b2d162e0486aeab
Igor Storchevoy <hellsgor@gmail.com>
2024-12-03 01:31:13 +0400

    Update[data/posts]: updated content

--- src/assets/data/posts.tsx
+++ src/assets/data/posts.tsx
@@ -2,19 +2,18 @@ import { Post } from '../../components/PostItem/PostItem';
 
 export const initialPosts: Post[] = [
   {
-    id: 1,
-    title:
-      'sunt aut facere repellat provident occaecati excepturi optio reprehenderit',
-    body: 'quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto',
+    id: 1730419200000,
+    title: 'The Rise of JavaScript',
+    body: 'JavaScript is a versatile programming language that powers modern web development.',
   },
   {
-    id: 2,
-    title: 'qui est esse',
-    body: 'est rerum tempore vitae\nsequi sint nihil reprehenderit dolor beatae ea dolores neque\nfugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis\nqui aperiam non debitis possimus qui neque nisi nulla',
+    id: 1730505600000,
+    title: 'Understanding React',
+    body: 'React is a popular library for building user interfaces, developed by Facebook.',
   },
   {
-    id: 3,
-    title: 'ea molestias quasi exercitationem repellat qui ipsa sit aut',
-    body: 'et iusto sed quo iure\nvoluptatem occaecati omnis eligendi aut ad\nvoluptatem doloribus vel accusantium quis pariatur\nmolestiae porro eius odio et labore et velit aut',
+    id: 1730592000000,
+    title: 'Exploring Node.js',
+    body: 'Node.js enables JavaScript to run on the server, making it a full-stack language.',
   },
 ];
