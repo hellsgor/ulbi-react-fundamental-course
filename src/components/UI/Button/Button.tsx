76f4aefe659847174619cd1b3406fef5c3cdd757
Igor Storchevoy <hellsgor@gmail.com>
2024-12-03 00:22:29 +0400

    Update[Button]: added modifier 'secondary'

--- src/components/UI/Button/Button.tsx
+++ src/components/UI/Button/Button.tsx
@@ -1,13 +1,20 @@
 import { ButtonHTMLAttributes, FC, ReactNode } from 'react';
 import classes from './Button.module.css';
 
+export type ButtonMods = 'secondary';
 export interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   children: ReactNode;
+  mods?: Array<ButtonMods>;
 }
 
-export const Button: FC<ButtonProps> = ({ children, ...props }) => {
+export const Button: FC<ButtonProps> = ({ children, mods, ...props }) => {
+  const classesStr: string = [
+    classes.button,
+    ...(mods?.map((mod) => classes[`button_${mod}`]) || []),
+  ].join(' ');
+
   return (
-    <button className={classes.button} {...props}>
+    <button className={classesStr} {...props}>
       {children}
     </button>
   );
