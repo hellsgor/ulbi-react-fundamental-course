6470a22dedece9a6df139fd69981b88242ad79c4
Igor Storchevoy <hellsgor@gmail.com>
2024-12-04 00:07:57 +0400

    Update[posts]: updated data

--- src/assets/data/posts.tsx
+++ src/assets/data/posts.tsx
@@ -8,8 +8,9 @@ export const initialPosts: Post[] = [
   },
   {
     id: 1730505600000,
-    title: 'Understanding React',
-    body: 'React is a popular library for building user interfaces, developed by Facebook.',
+    title:
+      'Comprehensive Guide to Understanding React: The Building Block of Modern User Interfaces',
+    body: 'React is a highly popular JavaScript library for building interactive and dynamic user interfaces, initially developed and maintained by Facebook. It is widely used for creating complex web applications, thanks to its component-based architecture and virtual DOM, which ensure efficient updates and rendering. Since its launch in 2013, React has become a go-to choice for developers worldwide, enabling them to build reusable UI components that manage their own state. With a vast ecosystem of tools, libraries, and community support, React continues to dominate the front-end development space, powering websites and applications for companies of all sizes across diverse industries.',
   },
   {
     id: 1730592000000,
