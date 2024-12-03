e21358750336bb593db9bb87105d139b45d79f3f
Igor Storchevoy <hellsgor@gmail.com>
2024-12-04 02:24:17 +0400

    Feat[Select]: create component

---
+++ src/components/UI/Select/Select.tsx
@@ -0,0 +1,48 @@
+import classes from './Select.module.css';
+
+export type SelectOption = { value: string; text: string };
+
+export interface SelectProps<T> {
+  options: SelectOption[];
+  defaultValue?: SelectOption | null;
+  label?: string | null;
+  id?: string | null;
+  name?: string | null;
+  value: T;
+  onChange: (value: T) => void;
+}
+
+export const Select = <T extends string>({
+  options,
+  value,
+  onChange,
+  defaultValue = null,
+  label = null,
+  id = null,
+  name = null,
+}: SelectProps<T>) => {
+  const ids = `select-${id ? id : Math.floor(Math.random() * 1001)}`;
+
+  return (
+    <div className={classes.select}>
+      {label && <label htmlFor={ids}>{label}</label>}
+
+      <select
+        name={name || ids}
+        id={ids}
+        value={value}
+        onChange={(event) => onChange(event.target.value as T)}
+      >
+        {defaultValue?.value && defaultValue?.text && (
+          <option value={defaultValue.value}>{defaultValue.text}</option>
+        )}
+
+        {options.map(({ value, text }) => (
+          <option key={value} value={value}>
+            {text}
+          </option>
+        ))}
+      </select>
+    </div>
+  );
+};
