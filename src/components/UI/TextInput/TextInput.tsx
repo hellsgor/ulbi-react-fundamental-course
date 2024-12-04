f8cd257f700a027f734a1c3587e2dbf5a4aa335b
Igor Storchevoy <hellsgor@gmail.com>
2024-12-05 02:44:12 +0400

    Update[TextInput]: added modifiers, label, id processing

--- src/components/UI/TextInput/TextInput.tsx
+++ src/components/UI/TextInput/TextInput.tsx
@@ -1,6 +1,35 @@
-import { InputHTMLAttributes, FC } from 'react';
 import classes from './TextInput.module.css';
 
-export const TextInput: FC<InputHTMLAttributes<HTMLInputElement>> = (props) => {
-  return <input {...props} className={classes['text-input']} />;
+import { InputHTMLAttributes, FC } from 'react';
+import { generateId } from '../../../utils/generateId';
+import { generateClassNames } from '../../../utils/generateClassNames';
+
+export type TextInputModifiers = 'small' | 'horizontal';
+
+export interface TextInputProps extends InputHTMLAttributes<HTMLInputElement> {
+  label?: string;
+  id?: string;
+  mods?: Record<TextInputModifiers, boolean>;
+}
+
+export const TextInput: FC<TextInputProps> = ({
+  label,
+  id,
+  mods,
+  ...restProps
+}) => {
+  const ids: string = id ? id.toString() : generateId('text-input', id);
+
+  return (
+    <div
+      className={generateClassNames({
+        baseClass: classes['text-input'],
+        classes,
+        mods,
+      })}
+    >
+      {label && <label htmlFor={ids}>{label}</label>}
+      <input {...restProps} id={ids} />
+    </div>
+  );
 };
