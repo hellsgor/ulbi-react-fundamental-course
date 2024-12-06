3dac70e8e2f7d7784ae58556eabfb65000d7ccb9
Igor Storchevoy <hellsgor@gmail.com>
2024-12-07 03:04:43 +0400

    Update[Select]: updated for modifiers

--- src/components/UI/Select/Select.tsx
+++ src/components/UI/Select/Select.tsx
@@ -3,12 +3,15 @@ import classes from './Select.module.css';
 
 export type SelectOption = { value: string; text: string };
 
+export type SelectMods = 'small';
+
 export interface SelectProps<T> {
   options: SelectOption[];
   defaultValue?: SelectOption | null;
   label?: string | null;
   id?: string | null;
   name?: string | null;
+  mods?: Partial<Record<SelectMods, true>>;
   value: T;
   onChange: (value: T) => void;
 }
@@ -21,11 +24,16 @@ export const Select = <T extends string>({
   label = null,
   id = null,
   name = null,
+  mods,
 }: SelectProps<T>) => {
   const ids = id ? id.toString() : generateId('select', id);
 
+  const rootClass = mods
+    ? [classes.select].concat(Object.keys(mods).map((key) => classes[key]))
+    : [classes.select];
+
   return (
-    <div className={classes.select}>
+    <div className={rootClass.join(' ')}>
       {label && <label htmlFor={ids}>{label}</label>}
 
       <select
