8e2fa452f4673156493baa47589f315da8d649ce
Igor Storchevoy <hellsgor@gmail.com>
2024-12-09 02:15:57 +0400

    FIx[PostService]: fixed url

--- src/api/PostService.tsx
+++ src/api/PostService.tsx
@@ -4,7 +4,7 @@ import { PostList, PostListSchema } from '../types/Post';
 export default class PostService {
   static async getAll(): Promise<PostList> {
     return await axios
-      .get('https://jsonplaceholder.typicode.com/pos')
+      .get('https://jsonplaceholder.typicode.com/posts')
       .then((response) => {
         if (response.status >= 400) throw new Error();
         return response;
