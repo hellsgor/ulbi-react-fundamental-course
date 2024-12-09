6a0e0bd7daf3f92774fd59b4e898ee630698e224
Igor Storchevoy <hellsgor@gmail.com>
2024-12-10 00:57:35 +0400

    Update[Button]: added className prop

--- src/components/UI/Button/Button.tsx
+++ src/components/UI/Button/Button.tsx
@@ -7,18 +7,28 @@ export type ButtonMods = 'secondary' | 'wide' | 'transparent';
 export interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   children: ReactNode;
   mods?: Partial<Record<ButtonMods, boolean>>;
+  className?: string;
 }
 
-export const Button: FC<ButtonProps> = ({ children, mods, ...props }) => {
+export const Button: FC<ButtonProps> = ({
+  children,
+  mods,
+  className,
+  ...props
+}) => {
+  const classNames = [
+    getClassNames({
+      classes,
+      root: classes.button,
+      mods,
+    }),
+    className,
+  ]
+    .filter((className) => !!className)
+    .join(' ');
+
   return (
-    <button
-      className={getClassNames({
-        classes,
-        root: classes.button,
-        mods,
-      })}
-      {...props}
-    >
+    <button className={classNames} {...props}>
       {children}
     </button>
   );
