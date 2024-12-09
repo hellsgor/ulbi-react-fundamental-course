bfd2f4d3bf824d00e9a0e367e3a75f809ac384a6
Igor Storchevoy <hellsgor@gmail.com>
2024-12-09 22:58:38 +0400

    Update[PostService]: updated getAll method for pagination

--- src/api/PostService.tsx
+++ src/api/PostService.tsx
@@ -1,14 +1,35 @@
 import axios from 'axios';
 import { PostList, PostListSchema } from '../types/Post';
 
+export interface PostServiceResponse {
+  data: PostList;
+  postsCount: string;
+}
+
 export default class PostService {
-  static async getAll(): Promise<PostList> {
-    return await axios
-      .get('https://jsonplaceholder.typicode.com/posts')
+  static async getAll(
+    limit: number = 10,
+    page: number = 1,
+  ): Promise<PostServiceResponse> {
+    const response = await axios
+      .get('https://jsonplaceholder.typicode.com/posts', {
+        params: {
+          _limit: limit,
+          _page: page,
+        },
+      })
       .then((response) => {
         if (response.status >= 400) throw new Error();
         return response;
       })
-      .then(({ data }) => PostListSchema.parse(data));
+      .then((response) => {
+        PostListSchema.parse(response.data);
+        return {
+          data: response.data,
+          postsCount: response.headers['x-total-count'],
+        };
+      });
+
+    return response;
   }
 }
