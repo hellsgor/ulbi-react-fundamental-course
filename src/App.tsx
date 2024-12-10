1807ef7b724bd95fa2f02b51a1472ebc2af7a79a
Igor Storchevoy <hellsgor@gmail.com>
2024-12-11 01:23:50 +0400

    Feat[App]: implemented unknown path handling

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import './styles/App.css';
 
-import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 import Posts from './pages/Posts';
 import About from './pages/About';
 import { Header } from './components/Header/Header';
+import { Error } from './pages/Error';
 
 function App() {
   return (
@@ -14,7 +15,8 @@ function App() {
         <Routes>
           <Route path="/about" element={<About />} />
           <Route path="/posts" element={<Posts />} />
-          <Route path="*" element={<Navigate to="/posts" replace />} />
+          {/* <Route path="/error" element={<Error />} /> */}
+          <Route path="*" element={<Error />} />
         </Routes>
       </main>
     </BrowserRouter>
