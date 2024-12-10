abe922f5cdd6defe736ff0559d0e94e13b974628
Igor Storchevoy <hellsgor@gmail.com>
2024-12-10 23:39:50 +0400

    Feat[App]: added routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,78 +1,22 @@
 import './styles/App.css';
 
-import { PostList, PostListFilter } from './components/PostList/PostList';
-import { PostForm } from './components/PostForm/PostForm';
-import { useEffect, useState } from 'react';
-import { Modal } from './components/Modal/Modal';
-import { usePosts } from './hooks/usePosts';
-import { Post } from './types/Post';
-import PostService from './api/PostService';
-import { useFetching } from './hooks/useFetching';
-import { getPagesCount } from './utils/getPagesCount';
-import { usePagination } from './hooks/usePagination';
-import { Pagination } from './components/Pagination/Pagination';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+import Posts from './pages/Posts';
+import About from './pages/About';
+import { Header } from './components/Header/Header';
 
 function App() {
-  const [posts, setPosts] = useState<Post[]>([]);
-  const [filter, setFilter] = useState<PostListFilter>({
-    sort: 'id',
-    query: '',
-  });
-  const [totalPages, setTotalPages] = useState(0);
-  const [limit, setLimit] = useState(10);
-  const [page, setPage] = useState(1);
-  const [modal, setModal] = useState(false);
-  const foundPosts = usePosts(posts, filter);
-
-  const [fetchPosts, isPostsLoading, postError] = useFetching(
-    async (limit: number, page: number) => {
-      const response = await PostService.getAll(limit, page);
-      setPosts(response.data);
-      setTotalPages(getPagesCount(response.postsCount, limit));
-    },
-  );
-
-  useEffect(() => {
-    fetchPosts(limit, page);
-  }, []);
-
-  function createPost(newPost: Post): void {
-    setPosts([...posts, newPost]);
-    setModal(false);
-  }
-
-  function removePost(post: Post) {
-    setPosts(posts.filter((p) => p.id !== post.id));
-  }
-
-  function changePage(page: number) {
-    setPage(page);
-    fetchPosts(limit, page);
-  }
-
   return (
-    <div className="App">
-      <Modal visible={modal} setVisible={setModal}>
-        <PostForm create={createPost} />
-      </Modal>
-
-      <PostList
-        remove={removePost}
-        list={foundPosts}
-        title="Список постов"
-        filter={filter}
-        setFilter={setFilter}
-        setFormVisible={setModal}
-        loading={isPostsLoading}
-        error={postError}
-      />
-
-      <Pagination
-        pages={usePagination(totalPages)}
-        current={page}
-        setPage={changePage}
-      />
-    </div>
+    <BrowserRouter>
+      <Header />
+      <main>
+        <Routes>
+          <Route path="/about" element={<About />}></Route>
+          <Route path="/posts" element={<Posts />}></Route>
+        </Routes>
+      </main>
+    </BrowserRouter>
   );
 }
 
