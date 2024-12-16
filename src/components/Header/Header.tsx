699546e56531698aac22badc4165e9058fc196ea
Igor Storchevoy <hellsgor@gmail.com>
2024-12-17 00:41:11 +0400

    Update[Header]: updated main navigation menu items & styles

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,15 +1,24 @@
-import { NavLink, Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import classes from './Header.module.css';
 
 export type NavItem = {
   text: string;
   path: string;
+  attrs?: React.AnchorHTMLAttributes<HTMLAnchorElement>;
+  mods?: string[];
 };
 
 export const Header = () => {
   const navItems: NavItem[] = [
-    { text: 'About', path: '/about' },
+    { text: 'About', path: 'about' },
     { text: 'Posts', path: 'posts' },
+    {
+      text: 'Visit chanel',
+      path: 'https://www.youtube.com/@UlbiTV',
+      attrs: { target: '_blank' },
+      mods: [classes.wide, classes.external],
+    },
+    { text: 'Login', path: 'login', mods: [classes.right] },
   ];
 
   return (
@@ -18,30 +27,25 @@ export const Header = () => {
         <div className={classes.headerWrapper}>
           <div className={classes.headerLogo}></div>
           <nav className={classes.headerNav}>
-            {navItems.map((item) => (
-              <NavLink
-                key={item.text}
-                to={item.path}
-                className={({ isActive }) =>
-                  isActive
-                    ? `${classes.headerNavItem} ${classes.active}`
-                    : classes.headerNavItem
-                }
-                end
-              >
-                {item.text}
-              </NavLink>
-            ))}
+            {navItems.map((item) => {
+              const classNames = item.mods?.length
+                ? [classes.headerNavItem, ...item.mods].join(' ')
+                : classes.headerNavItem;
+              return (
+                <NavLink
+                  key={item.text}
+                  to={item.path}
+                  className={({ isActive }) =>
+                    isActive ? `${classNames} ${classes.active}` : classNames
+                  }
+                  {...item.attrs}
+                  end
+                >
+                  {item.text}
+                </NavLink>
+              );
+            })}
           </nav>
-          <div className={classes.headerContacts}>
-            <Link
-              target="_blank"
-              to="https://www.youtube.com/@UlbiTV"
-              className={classes.headerNavItem}
-            >
-              visit channel
-            </Link>
-          </div>
         </div>
       </div>
     </header>
