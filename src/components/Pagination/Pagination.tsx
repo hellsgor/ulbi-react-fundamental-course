3f8e39df6fe175e22a0261e9535ff361af8dc873
Igor Storchevoy <hellsgor@gmail.com>
2024-12-20 01:57:45 +0400

    Feat[Pagination]: added posts limit selector

--- src/components/Pagination/Pagination.tsx
+++ src/components/Pagination/Pagination.tsx
@@ -2,32 +2,59 @@ import classes from './Pagination.module.css';
 
 import { FC } from 'react';
 import { Button } from '../UI/Button/Button';
+import { Select } from '../UI/Select/Select';
 
 export type PaginationProps = {
   pages: number[];
   current: number;
   setPage: (value: number) => void;
+  limit: number;
+  changeLimit: (value: number) => void;
 };
 
 export const Pagination: FC<PaginationProps> = ({
   pages,
   current,
   setPage,
+  limit,
+  changeLimit,
 }) => {
+  function setPaginationLimit(limit: string) {
+    changeLimit(Number(limit));
+  }
+
+  if (!pages.length) {
+    pages = [1];
+  }
+
   return (
     <div className={classes.pagination}>
-      {pages.map((page) => (
-        <Button
-          key={page}
-          mods={{ transparent: true }}
-          type="button"
-          className={page === current ? classes.active : ''}
-          disabled={page === current}
-          onClick={() => setPage(page)}
-        >
-          {page}
-        </Button>
-      ))}
+      <div className={classes.pages}>
+        {pages.map((page) => (
+          <Button
+            key={page}
+            mods={{ transparent: true }}
+            type="button"
+            className={page === current ? classes.active : ''}
+            disabled={page === current}
+            onClick={() => setPage(page)}
+          >
+            {page}
+          </Button>
+        ))}
+      </div>
+
+      <Select
+        label={'Download by: '}
+        mods={{ small: true }}
+        options={[
+          { value: '20', text: '20' },
+          { value: '-1', text: 'all' },
+        ]}
+        defaultValue={{ value: '10', text: '10' }}
+        value={`${limit}`}
+        onChange={setPaginationLimit}
+      />
     </div>
   );
 };
